d59e3b24735fcc6b16d0e76501e21c3f7dc4e94c
Adrian Yorke <adrianyorke@gmail.com>
2018-12-02 02:11:29 +0200

    Updated app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import datetime
 from flask import Flask
 from buzz import generator
 
+# https://coder-coder.com/how-to-make-simple-website-html/
+
 app = Flask(__name__)
 
 signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
@@ -17,7 +19,13 @@ def generate_buzz():
     page += '<head>'
     page += '<meta charset="utf-8">'
     page += '<meta name="viewport" content="width=device-width, initial-scale=1">'
+    page += '<meta name="author" content="Adrian Yorke">'
     page += '<title>DevOps Buzz Generator</title>'
+    #page += '<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">'
+    #page += '<link rel="shortcut icon" type="image/x-icon" href={{ url_for("images", filename="favicon.ico") }}>'
+    #page += '<link rel="icon" href="/images/favicon-32x32.png">'
+    #page += '<link rel="shortcut icon" href="/favicon/devops_cycle_1_flat-512.png">'
+    #page += '<link rel="icon" type="image/png" href="/favicon/devops_cycle_1_flat-512.png">'
     page += '</head>'
     
     page += '<body>'
